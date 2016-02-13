dfed90234902977a7b67f2f3c48efdba3ca834df
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-02-14 02:12:43 +0300

    Remove system function

--- src/gpio.c
+++ src/gpio.c
@@ -3,25 +3,6 @@
 
 static PyObject *GpioError;
 
-static PyObject * gpio_system(PyObject *self, PyObject *args)
-{
-	const char *command;
-	int sts;
-
-	if (!PyArg_ParseTuple(args, "s", &command))
-		return NULL;
-
-	sts = system(command);
-
-	if (sts < 0)
-	{
-		PyErr_SetString(GpioError, "System command failed");
-		return NULL;
-	}
-
-	return PyLong_FromLong(sts);
-}
-
 static PyObject * gpio_direction(PyObject *self, PyObject *args, PyObject *keywds)
 {
 	const int gpio;
@@ -38,7 +19,6 @@ static PyObject * gpio_direction(PyObject *self, PyObject *args, PyObject *keywd
 }
 
 static PyMethodDef GpioMethods[] = {
-		{"system", gpio_system, METH_VARARGS, "Execute a shell command."},
 		{"direction", (PyCFunction)gpio_direction, METH_VARARGS|METH_KEYWORDS, "Set direction of a gpio"},
 		{NULL, NULL, 0, NULL}
 };
